c1ef94af92c8ec9478f0a3ef76e00b892f71fbc5
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-08-23 23:19:41 +0300

    Initial Commit

---
+++ FnnyCircles/FnnyCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FnnyCirclesApp.swift
+//  FnnyCircles
+//
+//  Created by Vladimir Dvornikov on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FnnyCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
